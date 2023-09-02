564946469881181735628052858fa4c2385111f2
DmitryStarov <314263@bk.ru>
2023-09-03 00:51:01 +0800

    feat/Navigation

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,16 +1,15 @@
 import './Header.css';
 import { Link } from 'react-router-dom';
 import logo from '../../image/logo.svg';
-import AuthNavigation from '../AuthNavigation/AuthNavigation';
+// import AuthNavigation from '../AuthNavigation/AuthNavigation';
+import Navigation from '../Navigation/Navigation';
 
 const Header = () => (
   <header className="header">
-    <div className="header__container">
-      <Link to="/" className="header__link">
-        <img className="header__logo" src={logo} alt="логотип" />
-      </Link>
-    </div>
-    <AuthNavigation />
+    <Link to="/" className="header__link">
+      <img className="header__logo" src={logo} alt="логотип" />
+    </Link>
+    <Navigation />
   </header>
 );
 export default Header;
