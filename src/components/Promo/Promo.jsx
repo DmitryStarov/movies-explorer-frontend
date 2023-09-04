d96265fa4be0bb20eeb9ca7dddba764ab9123842
DmitryStarov <314263@bk.ru>
2023-09-04 23:55:57 +0800

    feat/Promo

--- src/components/Promo/Promo.jsx
+++ src/components/Promo/Promo.jsx
@@ -1,8 +1,10 @@
 import './Promo.css';
+import NavTab from '../NavTab/NavTab';
 
 const Promo = () => (
   <section className="promo" id="promo">
     <h1 className="promo__title">Учебный проект студента факультета Веб-разработки.</h1>
+    <NavTab />
   </section>
 );
 
