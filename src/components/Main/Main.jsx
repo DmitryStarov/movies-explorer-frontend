acb45a49f266a7912a972ff7cd4eb814d3fa537a
DmitryStarov <314263@bk.ru>
2023-09-04 22:43:15 +0800

    refactoring/Header

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,11 +1,14 @@
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
+import Promo from '../Promo/Promo';
 import './Main.css';
 
 const Main = ({ isLoggedIn }) => (
   <>
     <Header isLoggedIn={isLoggedIn} />
-
+    <main>
+      <Promo />
+    </main>
     <Footer />
   </>
 );
