d4c12f8748bd05b58c3467754ecebddc1d7ae41b
DmitryStarov <314263@bk.ru>
2023-09-10 17:08:38 +0800

    feat/PageNotFound

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,8 +1,20 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Main from '../Main/Main';
+import PageNotFound from '../PageNotFound/PageNotFound';
 
 const App = () => (
-  <Main />
+  <Routes>
+    <Route
+      path="/"
+      element={<Main />}
+    />
+    <Route
+      path="*"
+      element={<PageNotFound />}
+    />
+  </Routes>
+
 );
 
 export default App;
