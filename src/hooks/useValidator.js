961992c87ba076d579b30014e4b95803b6ebcb42
DmitryStarov <314263@bk.ru>
2023-09-10 22:42:45 +0800

    feat/useValidator

---
+++ src/hooks/useValidator.js
@@ -0,0 +1,29 @@
+import { useState } from 'react';
+
+export default function useValidator(values = {}) {
+  const [inputValues, setInputValues] = useState(values);
+  const [isValid, setIsValid] = useState(false);
+  const [errors, setErrors] = useState('');
+
+  function handleChange(evt) {
+    const { value, name } = evt.target;
+    setInputValues({ ...inputValues, [name]: value });
+    setErrors({ ...errors, [name]: evt.target.validationMessage });
+    setIsValid(evt.target.closest('.form').checkValidity());
+  }
+
+  function resetValidation() {
+    setInputValues({});
+    setIsValid(false);
+    setErrors({});
+  }
+
+  return {
+    inputValues,
+    setInputValues,
+    errors,
+    handleChange,
+    resetValidation,
+    isValid,
+  };
+}
