f6a80e4250654e6932449ea87a68053ba5de532a
DmitryStarov <314263@bk.ru>
2023-09-10 22:14:26 +0800

    feat/Auth

---
+++ src/components/Auth/Auth.jsx
@@ -0,0 +1,46 @@
+import './Auth.css';
+import { Link } from 'react-router-dom';
+import logo from '../../image/logo.svg';
+
+const Authentication = ({
+  title,
+  formName,
+  buttonText,
+  children,
+  errorText,
+  paragraphText,
+  paragraphLink,
+  paragraphButton,
+  isValid,
+}) => (
+  <main className="auth">
+    <div className="auth__container">
+      <Link to="/" className="auth__main-link button-hover">
+        <img className="auth__logo" src={logo} alt="логотип" />
+      </Link>
+      <h1 className="auth__title">{title}</h1>
+      <form className="auth__form" name={formName}>
+        {children}
+        <p className="auth__request-error">{errorText}</p>
+        <button
+          className="auth__submit-btn button-hover"
+          disabled={!isValid}
+          type="submit"
+        >
+          {buttonText}
+        </button>
+      </form>
+      <p className="auth__paragraph">
+        {paragraphText}
+        <Link
+          className="auth__link link-hover"
+          to={paragraphLink}
+        >
+          {paragraphButton}
+        </Link>
+      </p>
+    </div>
+  </main>
+);
+
+export default Authentication;
